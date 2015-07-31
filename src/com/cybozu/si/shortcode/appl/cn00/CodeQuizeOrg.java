b17447f4b3b9078d7f5ee2b56c78574275d4cee2
Koji Sato <kojisato2002@gmail.com>
2015-07-31 18:51:36 +0900

    Add Code Quize Test

--- src/com/cybozu/si/shortcode/appl/cn00/CodeQuizeOrg.java
+++ src/com/cybozu/si/shortcode/appl/cn00/CodeQuizeOrg.java
@@ -2,40 +2,51 @@ package com.cybozu.si.shortcode.appl.cn00;
 
 public class CodeQuizeOrg {
 
+
+	// 二次元座標の2点座標情報から距離を計算する
+
 	public static final int distance2dimension(int ax, int ay, int bx, int by) {
 
+		// x, y の一致
 		if(ax==bx) {
+			// y軸も一致
 			if(ay==by) {
+				// x, y 共に一致している場合の距離は0
 				return 0;
 			} else {
+				// x軸のみ不一致なので、yの差を計算して返却
 				return Math.abs(ay - by);
 			}
+		// x軸は不一致
 		} else {
+			// y軸は一致
 			if(ay==by) {
+				// y軸のみの一致なので、xの差を計算して返却
 				return Math.abs(ax - bx);
 			} else {
+				// x, y 共に不一致なので、計算関数に移譲
 				return calcPythagorean(ax, ay, bx, by);
 			}
 		}
 	}
 
 	private static int calcPythagorean(int ax, int ay, int bx, int by) {
-		
+
 		int x, y;
-		
+		// x軸の距離を計算
 		if(ax > bx) {
 			x = ax - bx;
 		} else {
 			x = bx - ax;
 		}
+		// y軸の距離を計算
 		if(ay > by) {
 			y = ay - by;
 		} else {
 			y = by - ay;
 		}
-		
-		return x + y;
+		// Pythagorean Calculate
+		return (int) Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
 	}
-
-
 }
--- src/com/cybozu/si/shortcode/test/cn00/testCodeQuizeOrg.java
+++ src/com/cybozu/si/shortcode/test/cn00/testCodeQuizeOrg.java
@@ -1,4 +1,4 @@
-package com.cybozu.si.shortcode.test.cn01;
+package com.cybozu.si.shortcode.test.cn00;
 
 import static org.junit.Assert.*;
 
@@ -28,17 +28,31 @@ public class testCodeQuizeOrg {
 	public void tearDown() throws Exception {
 	}
 
+
+	// 二次元座標の2点座標情報から距離を計算するテスト
 	@Test
 	public void testBasicCondition() {
+		// 同一座標のケース
 		assertEquals(0, CodeQuizeOrg.distance2dimension(10,10,10,10)); // (ax, ay)     == (bx , by)
+		// x座標は同一で、by座標のみ異なる
 		assertEquals(5, CodeQuizeOrg.distance2dimension(10,10,10,5));  // (ax, ay)     <> (bx , by -5)
+		// x座標は同一で、ay座標のみ異なる
 		assertEquals(6, CodeQuizeOrg.distance2dimension(10,4,10,10));  // (ax, ay - 4) <> (bx, by)
+		// y座標は同一で、bx座標のみ異なる
 		assertEquals(7, CodeQuizeOrg.distance2dimension(10,10,3,10));  // (ax, ay)     <> (bx - 3, by)
+		// y座標は同一で、ax座標のみ異なる
 		assertEquals(8, CodeQuizeOrg.distance2dimension(2,10,10,10));  // (ax - 2, ay) <> (bx, by)
 	}
 	@Test
 	public void testPythagorean() {
-		assertEquals(60, CodeQuizeOrg.distance2dimension(-10,-10,20,20));
+		// 共に第1象限にある
+		assertEquals(14, CodeQuizeOrg.distance2dimension(10,10,20,20));
+		// 第1象限と第2象限(xが負)にある
+		assertEquals(31, CodeQuizeOrg.distance2dimension(-10,10,20,20));
+		// 第1象限と第3象限(ともに負)にある
+		assertEquals(42, CodeQuizeOrg.distance2dimension(-10,-10,20,20));
+		// 第1象限と第4象限(yが負)にある
+		assertEquals(31, CodeQuizeOrg.distance2dimension(10,-10,20,20));
 	}
 
 }
